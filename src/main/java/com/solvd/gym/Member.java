5f50d88c2ffe82ce2c42876c3c10d2845937ab47
Saeid Vahidnia <saeidvn@gmail.com>
2021-11-21 21:19:09 +0300

    remove logger

--- src/main/java/com/solvd/gym/Member.java
+++ src/main/java/com/solvd/gym/Member.java
@@ -4,13 +4,10 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
-import java.util.logging.LogManager;
 
 @XmlRootElement(name = "member")
 @XmlAccessorType (XmlAccessType.FIELD)
 public class Member {
-
-    private static final LogManager LOGGER = LogManager.getLogManager();
 
     @XmlAttribute()
     private Integer id;
